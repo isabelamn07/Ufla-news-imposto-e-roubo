c26d682d7a198a9a1959f9be9ca8d8dd01f0371b
isabela.nascimento <isabela.nascimento.lemaf@gmail.com>
2019-11-01 11:53:02 -0300

    Criaçao da activity de lista de publicadores

---
+++ src/app/lista-publicadores/lista-publicadores.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-publicadores',
+  templateUrl: './lista-publicadores.page.html',
+  styleUrls: ['./lista-publicadores.page.scss'],
+})
+export class ListaPublicadoresPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
