9b9d6a69676549971a1bbc12fefeb8d5c454af11
Tarik-INC <tarikesmin@gmail.com>
2019-11-04 00:10:57 -0300

    Finalizada validação do formulário de login, interface concluida, autentificação concluida, close #33, close #25

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { ToastController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +9,44 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  // private loggedUser: User = {
+  //   email: '',
+  //   password: ''
+  // };
 
-  constructor(private authService: AuthService) { }
+  private formGroup: FormGroup;
 
-  ngOnInit() {
+  constructor(private authService: AuthService, private toastCtrl: ToastController, private formBuilder: FormBuilder) { 
+    this.formGroup = formBuilder.group({
+      'email': [null, Validators.compose([
+        Validators.required, Validators.email
+      ])],
+      'passwd': [null,  Validators.compose([
+        Validators.required, Validators.minLength(6)
+      ])]
+    });
   }
 
- async login(form) {
-   console.log(form);
-   let resp = await this.authService.login(form.value)
+  ngOnInit() {
+
+  }
+
+  async login() {
+    console.log(this.formGroup.value);
+
+    try {
+      await this.authService.login(this.formGroup.value);
+    } catch (e) {
+      console.log(e);
+      let toast = await this.toastCtrl.create({
+        message: "E-mail ou senha incorretos!",
+        duration: 2000,
+        color: "danger"
+      })
+      toast.present();
+    }
+
+    return;
   }
 
 }
